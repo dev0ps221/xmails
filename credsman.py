1303c02edcb44da78822e8756c8f22a95db8bc32
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-31 21:11:33 +0000

    basic functions and stuff | will refactor later.................... .. . .... ... haha

--- credsman.py
+++ credsman.py
@@ -2,4 +2,16 @@
 
 from krypt import code as kode,d_code as dkode
 
-print(kode('123'))
+def ask_creds():
+    user = ask_and_code('user')
+    pwd =  ask_and_code()
+    return user,pwd
+
+def ask_and_code(val):
+    return kode(getpass.getuser()) if val = 'user' else kode(getpass.getpass())
+
+def generate_creds_file(gui=False,page=None):
+    if not gui:
+        usr,pwd = ask_creds()
+        print(usr,pwd)
+        
