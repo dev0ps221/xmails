e4814f1db3d86ad31c20deb3acc38af8ccfabf9d
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-01 10:47:31 +0000

    get creds profiles | creds stuff

--- credsman.py
+++ credsman.py
@@ -14,8 +14,17 @@ def get_creds_files():
     from os import listdir
     filelist = listdir(credspath)
     credsfiles = [elem for elem in filter(isnt_none,map(has_credsext,filelist))]
-    print(credsfiles)
     return credsfiles
+
+def get_creds_file(profile):
+    credsfiles = get_creds_files()
+    def matches(filename):
+        return f"{profile}.xcreds" == filename
+    match_ = [elem for elem in filter(matches,credsfiles)]
+    return f"{credspath}/{match_[0]}" if len(match_) else None
+
+def get_creds_profiles():
+    return [profile.replace('.xcreds','') for profile in get_creds_files()]
 
 def ask_creds():
     user = ask_and_code('user')
@@ -53,29 +62,40 @@ def get_creds(profile=None):
         print('to which is the username do want to get related creds')
         profile = input('answer>')
     if profile in get_creds_profiles():
-
+        return decode_creds_file(get_creds_file(profile)) 
+    else :
+        print('sorry, but the requested profile {} doesnt exist'.format(profile))
+        return None
     
 
-def action_menu():
-    print("what do you want to do ?")
-    print(" 1 - generate creds file")    
-    print(" 2 - read creds file")
-    print(" 0 - exit")
+if(__name__ == '__main__'):
     actions = {
         '0': exit,
         '1': generate_creds_file,
         '2': get_creds
     }
-    choice = input("answer>")
-    try:
-        choice = int(choice)
-    except Exception as e:
-        print(e)
-        err = True
-    finally:
-        if not err :        
-            if str(choice) in '012' 
-if(__name__ == '__main__'):
-
+    def action_menu():
+        def ask_action():
+            print("what do you want to do ?")
+            print(" 1 - generate creds file")    
+            print(" 2 - read creds file")
+            print(" 0 - exit")
+            choice = input("answer>")
+            err = None
+            try:
+                choice = int(choice)
+            except Exception as e:
+                print(e)
+                err = True
+            finally:
+                if not err :        
+                    choice = str(choice)
+                    if choice in actions:
+                        return actions[choice]() 
+                    else:
+                        print('wrong choice')
+                        ask_action()
+        ask_action()
+    action_menu()
     # credsfile = generate_creds_file()
     # get_creds_files()
