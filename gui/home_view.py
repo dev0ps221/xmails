078b73b4454c8e184a190a51e0d93bc6997e2089
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-01 16:08:06 +0000

    done making it log in | focus is now the mailboxes content listing

--- gui/home_view.py
+++ gui/home_view.py
@@ -1,5 +1,11 @@
 from flet import app, TextField, Text, Column, Row, Page, ElevatedButton, colors, alignment, Dropdown, dropdown
+
+def mail_folders(server):
+    return [folder for folder in map(lambda folder:folder.decode().split(' "/" '),server.list()[1])]
+
 def home_view(page,imap_server,refresh_page,refresh_view,logout):    
+    mailboxes = mail_folders(imap_server)
+    print(mailboxes)
     pagewidth = int(page.__dict__['_Control__attrs']['windowwidth'][0].split('.')[0])
     pageheight = int(page.__dict__['_Control__attrs']['windowheight'][0].split('.')[0])
 
