2732e80b0019b131aca23d9385c832c615aa59f8
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-01 21:07:38 +0000

    done making it log in | focus is now the mailboxes content listing

--- gui/home_view.py
+++ gui/home_view.py
@@ -2,11 +2,17 @@ from flet import app, TextField, Text, Column, Row, Page, ElevatedButton, colors
 
 def mail_folders(server):
     boxes = [folder for folder in map(lambda folder:folder.decode().split(' "/" '),server.list()[1])]
+    mailboxes = []
     for box in boxes :
-        if box[1] == "[Gmail]": continue
+        if box[1] == '"[Gmail]"':
+            box.append(['',''])
+            box.append('')
+            continue
         resp_code, mail_count = server.select(box[1])
-        box.append([resp_code.decode(),mail_count.decode()])
-    return boxes
+        box.append([resp_code,mail_count])
+        box.append(box[1].replace('"','').split('/')[-1])
+        mailboxes.append(box)
+    return mailboxes
 def home_view(page,imap_server,refresh_page,refresh_view,logout):    
     mailboxes = mail_folders(imap_server)
     pagewidth = int(page.__dict__['_Control__attrs']['windowwidth'][0].split('.')[0])
@@ -15,9 +21,10 @@ def home_view(page,imap_server,refresh_page,refresh_view,logout):
     view = Column()
     mailbox_container = Row()
     boxlist = Column()
+    boxlist.width = int(int(pagewidth*30)/100)
     for box in mailboxes:
-        boxlist.controls.append(Text(value=f"{box[1]} ({box[2][1]})"))
-        
+        boxlist.controls.append(Text(value=f"{box[-1]} ({box[2][1]})"))
+
     mailbox_container.controls.append(boxlist)
     view.controls.append(mailbox_container)
     return view
