4a73cc48b5c34d3e7d1c3b508e2a5ec0787b170d
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-03 17:04:40 +0000

    deleted:    coreman.py 	deleted:    main.py 	deleted:    test 	deleted:    test.py 	renamed:    app.py -> xmail.py

--- coreman.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from managers.credsmanager import CredsManager
-
-credsman = CredsManager()
-credsprofiles = []
-for profile in credsman.get_creds_profiles():
-    if profile not in credsprofiles:
-        credsprofiles.append(profile)
-print(credsprofiles)
--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from flet import app
-from gui.xmail import app_loop
-
-
-
-
-app(target=app_loop)
