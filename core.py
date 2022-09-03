8f3ae35058a93065980474fd9021bbfe09f21a0c
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-03 07:55:54 +0000

    refactoring a bit | >-> switching to objects | Managers : nearly done on ConnectionManager | login stuff

--- core.py
+++ core.py
@@ -1,6 +1,1 @@
 #!/usr/bin/env python3
-from managers.credsmanager import CredsManager
-
-credsman = CredsManager()
-credsprofiles = credsman.get_creds_profiles()
-
--- gui/Login.py
+++ gui/Login.py
@@ -1,5 +1,9 @@
 from flet import TextField, Text, Column, Row, ElevatedButton, colors, alignment, Dropdown, dropdown
-from credsman import *
+from managers.credsmanager import CredsManager
+
+credsman = CredsManager()
+credsprofiles = credsman.get_creds_profiles()
+
 class Login:
 
     email = Column()
@@ -8,102 +12,105 @@ class Login:
     login = Column()
     profiles = Column()
     select_view = Row()
+    loginview = Column()
+    emaillabel = Text(value='Email')
+    emailInput = TextField(label='Email')
+    pwdlabel = Text(value='pwd')
+    pwdInput = TextField(label='pwd',password=True)
+    login_profiles_select = Dropdown(
+        label="PROFILES",
+        options=[],
+        hint_text="Choose the profile you want to log in",
+        autofocus=True
+    )
+        
 
-    
-    def __init__(page,profiles,refresh_page,refresh_view):
+    def __init__(self,page,profiles,refresh_page,refresh_view):
         self.page       = page
-        self.profiles   = profiles
+        self.login_profiles   = profiles
         self.refresh_page = lambda *a : refresh_page(*a)
         self.refresh_view = lambda *a : refresh_view(*a)
         self.pagewidth = int(self.page.__dict__['_Control__attrs']['windowwidth'][0].split('.')[0])
         self.pageheight = int(self.page.__dict__['_Control__attrs']['windowheight'][0].split('.')[0])
         self.actual_login_view = 'Login'
+        self.build_components()
 
     def build_components(self):
-    def loginbox():
-        email.horizontal_alignment ='center'
-        emaillabel = Text(value='Email')
-        emaillabel.width = int(pagewidth/2)
-        emaillabel.alignment = alignment.center
-        emailInput = TextField(label='Email')
-        emailInput.width = int(pagewidth/2)
-        emailInput.alignment = alignment.center
-        email.controls=[emaillabel,emailInput]
-        email.horizontal_alignment = "center"
+        self.loginview = loginbox(page)
+        self.profilesview = profilesbox(page)
+        
+
+    def loginbox(self):
+        self.email.horizontal_alignment ='center'
+        self.emaillabel.width = int(pagewidth/2)
+        self.emaillabel.alignment = alignment.center
+        self.emailInput.width = int(pagewidth/2)
+        self.emailInput.alignment = alignment.center
+        self.email.controls=[self.emaillabel,self.emailInput]
+        self.email.horizontal_alignment = "center"
         
         
-        pwdlabel = Text(value='pwd')
-        pwdlabel.width = int(pagewidth/2)
-        pwdInput = TextField(label='pwd',password=True)
-        pwdInput.width = int(pagewidth/2)
-        pwd.controls=[pwdlabel,pwdInput]
-        pwd.horizontal_alignment = "center"
+        self.pwdlabel.width = int(pagewidth/2)
+        self.pwdInput.width = int(pagewidth/2)
+        self.pwd.controls=[self.pwdlabel,self.pwdInput]
+        self.pwd.horizontal_alignment = "center"
 
-        loginview = Column()
-        loginview.width = pagewidth
-        loginview.horizontal_alignment ='center'
-        loginview.controls = [email,pwd]
-        return loginview
+        self.loginview.width = pagewidth
+        self.loginview.horizontal_alignment ='center'
+        self.loginview.controls = [email,pwd]
+        return self.loginview
 
-    def profilesbox():
-        profiles = [profile for profile in self.profiles]
-        pagewidth = int(page.__dict__['_Control__attrs']['windowwidth'][0].split('.')[0])
-        pageheight = int(page.__dict__['_Control__attrs']['windowheight'][0].split('.')[0])
-        login_profiles_select = Dropdown(
-            label="PROFILES",
-            hint_text="Choose the profile you want to log in",
-            options=profiles,
-            autofocus=True
-        )
-        login_profiles_select.width = int(pagewidth/2)
+    def profilesbox(self):
+        profiles = [profile for profile in self.login_profiles]
+        
+        self.login_profiles_select.options=profiles
+        self.login_profiles_select.width = int(self.pagewidth/2)
         
         
-        profilesview = Column()
-        profilesview.width = pagewidth
-        profilesview.horizontal_alignment ='center'
-        profilesview.controls = [login_profiles_select]
+        self.profilesview.width = pagewidth
+        self.profilesview.horizontal_alignment ='center'
+        self.profilesview.controls = [self.login_profiles_select]
 
-        return profilesview,login_profiles_select
-
+        return self.profilesview
 
 
-    def switch_login_view(event):
+
+    def switch_login_view(self,event):
         self.actual_login_view = event.data
-        self.refresh_view(page,imap_server)
+        self.refresh_view(self.page,self.server)
 
 
-    def build_view(refresh_page,refresh_view,login_success):
+    def do_login(self,event):
+        loginerror = None
+        usrval  = None
+        passval = None
+        if login_view.actual_login_view == 'Profiles':
+            credsinstance = credsman.get_creds_instance(self.login_profiles_select.value)
+            if credsinstance :
+                usrval = credsinstance.get_cred('user')
+                passval = credsinstance.get_cred('pass')
+        else:
+            usrval  = emailInput.value
+            passval = pwdInput.value
+            generate_creds_file(usrval,passval)
+        if usrval and passval:
+            try:
+                imap_server.login(usrval, passval)
+            except Exception as e:
+                self.loginerror = str(e).split('[AUTHENTICATIONFAILED]' if 'AUTHENTICATIONFAILED' in str(e) else ']')[1].replace('\'','') 
+            finally:
+                if loginerror:
+                    login_view.login_failed = loginerror
+                    refresh_view(page,imap_server)
+                else:
+                    login_success(page,usrval)
+                        
+
+    def build_view(self,refresh_page,refresh_view,login_success):
         self.page.clean()
         login_profiles = [elem for elem in map(dropdown.Option,get_creds_profiles())]
         login_profiles_select = None
 
-        def do_login(event):
-            loginerror = None
-            usrval  = None
-            passval = None
-            if login_view.actual_login_view == 'Profiles':
-                if login_profiles_select.value:
-                    creds = get_creds(login_profiles_select.value)
-                    if creds:
-                        usrval,passval = creds
-                    else:
-                        usrval,passval = (None,None)
-            else:
-                usrval  = emailInput.value
-                passval = pwdInput.value
-                generate_creds_file(usrval,passval)
-            if usrval and passval:
-                try:
-                    imap_server.login(usrval, passval)
-                except Exception as e:
-                    loginerror = str(e).split('[AUTHENTICATIONFAILED]' if 'AUTHENTICATIONFAILED' in str(e) else ']')[1].replace('\'','') 
-                finally:
-                    if loginerror:
-                        login_view.login_failed = loginerror
-                        refresh_view(page,imap_server)
-                    else:
-                        login_success(page,usrval)
-                        
 
         
         select_view.width = pagewidth/2
@@ -132,18 +139,15 @@ class Login:
         login.horizontal_alignment = "center"
 
 
-        loginview = loginbox(page)
-        
-        profilesview,login_profiles_select = profilesbox(page,login_profiles)
 
         if select_box.value == 'Login':
-            actual_login_view = loginview
+            self.actual_login_view = loginview
         else :
-            actual_login_view = profilesview
+            self.actual_login_view = profilesview
         
         if hasattr(login_view,'login_failed'): 
-            actual_login_view.controls.append(Text(value=login_view.login_failed))
+            self.actual_login_view.controls.append(Text(value=login_view.login_failed))
 
-        view.controls = [select_view,actual_login_view,login]
+        view.controls = [select_view,self.actual_login_view,login]
         
         return view
