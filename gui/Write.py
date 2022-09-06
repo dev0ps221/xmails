8b38a90258e486153fb9c718277bc6a66fb234fc
dev0ps221 <seybaxscorpion@gmail.com>
2022-09-06 00:12:32 +0000

    let's write mails | connectionmanager | configuring smtp stuff | message sent... now let's work a bit the visual

--- gui/Write.py
+++ gui/Write.py
@@ -21,7 +21,7 @@ class Write:
 
 
     def add_attachment(self,e: FilePickerResultEvent):
-        [self.attachments.append(f,filetype.guess(f.path)) for f in e.files]
+        [self.attachments.append((f,filetype.guess(f.path))) for f in e.files]
         print(self.attachments)
         return self.attachments
 
